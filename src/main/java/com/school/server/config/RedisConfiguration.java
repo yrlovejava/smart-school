5aeb363c3f53f948c44eee22c137b4e38a250a31
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-05-20 20:19:18 +0800

    配置redis

---
+++ src/main/java/com/school/server/config/RedisConfiguration.java
@@ -0,0 +1,35 @@
+package com.school.server.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    /**
+     * 配置redis
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        //设置redis连接
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置key序列化方式
+        // 设置key序列化方式string，RedisSerializer.string() 等价于 new StringRedisSerializer()
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式json，使用GenericJackson2JsonRedisSerializer替换默认序列化，RedisSerializer.json() 等价于 new GenericJackson2JsonRedisSerializer()
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+        return redisTemplate;
+    }
+}
