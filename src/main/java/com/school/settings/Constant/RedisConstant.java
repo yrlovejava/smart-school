3bab1763d540c37c007142c78dd6b90a1059e4d6
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-05-20 20:27:28 +0800

    redis配置

---
+++ src/main/java/com/school/settings/Constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package com.school.settings.Constant;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * redis相关的属性
+ */
+public class RedisConstant {
+    //验证码的有效时间
+    public static final long CODE_TIME_LIMIT_VALUE = 3;
+
+    //验证码有效时间的单位
+    public static final TimeUnit CODE_TIME_LIMIT_TYPE = TimeUnit.MINUTES;
+
+}
